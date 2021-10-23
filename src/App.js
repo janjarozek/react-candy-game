8e69a57b09a1865a0b01b46b5342524eeeb05d22
janjarozek <74878517+janjarozek@users.noreply.github.com>
2021-10-23 20:36:32 +0200

    random array of 64 colors placed once in state while component loads

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import React, {useState, useEffect} from 'react'
+
 const width = 8
 const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
 
 
 const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
   const createBoard = () => {
+    const randomColorArrangament = []
     for (let i=0; i< width * width; i++) {
+      // generate randome number 0 - length of array
       const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+      randomColorArrangament.push(randomColor)
     }
+    setCurrentColorArrangement(randomColorArrangament)
   }
+
+  useEffect(() => {
+    createBoard()
+  }, [])
+
+  console.log(currentColorArrangement)
 
   return (
     <div>
