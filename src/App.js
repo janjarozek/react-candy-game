c38456e9de273a2c94f87ed3901741ef56e9548a
janjarozek <74878517+janjarozek@users.noreply.github.com>
2021-10-23 21:11:37 +0200

    created the board and started checking for match of 3 and 4 columns

--- src/App.js
+++ src/App.js
@@ -7,6 +7,28 @@ const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
 const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
+  const checkForColumnOfThree = () => {
+    for(let i=0; i < (width *(width-2))-1; i++) {
+      const columnOfThree = [i, i+width, i+(width*2)];
+      const decidedColor = currentColorArrangement[i];
+
+      if ( columnOfThree.every( square => currentColorArrangement[square] === decidedColor)) {
+        columnOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  const checkForColumnOfFour = () => {
+    for(let i=0; i < (width *(width-3))-1; i++) {
+      const columnOfFour = [i, i+width, i+(width*2), i+(width*3)];
+      const decidedColor = currentColorArrangement[i];
+
+      if ( columnOfFour.every( square => currentColorArrangement[square] === decidedColor)) {
+        columnOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
   const createBoard = () => {
     const randomColorArrangament = []
     for (let i=0; i< width * width; i++) {
@@ -21,10 +43,26 @@ const App = () => {
     createBoard()
   }, [])
 
-  console.log(currentColorArrangement)
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree()
+      setCurrentColorArrangement([...currentColorArrangement])
+    }, 100)
+    return () => clearInterval(timer)
+  }, [checkForColumnOfThree, currentColorArrangement])
+
+  // console.log(currentColorArrangement)
 
   return (
-    <div>
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColor, index) => (
+          <img
+            key={`img-${index}`}
+            alt={candyColor}
+            style={{backgroundColor: candyColor}}></img>
+        ))}
+      </div>
     </div>
   );
 }
